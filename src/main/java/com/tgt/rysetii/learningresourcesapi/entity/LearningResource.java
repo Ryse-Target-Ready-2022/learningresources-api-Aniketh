e1ee8f75dae0e1eff73e81eaa6582668864aadc6
AnikethD <anikethdurs@gmail.com>
2022-07-24 11:58:15 +0530

    Added JPA with H2

--- src/main/java/com/tgt/rysetii/learningresourcesapi/entity/LearningResource.java
+++ src/main/java/com/tgt/rysetii/learningresourcesapi/entity/LearningResource.java
@@ -4,19 +4,32 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "learningresources")
 public class LearningResource {
+    @Id
+    @Column(name = "learning_resource_id")
     private Integer id;
+    @Column(name = "learning_resource_name")
     private String productName;
+    @Column(name = "cost_price")
     private Double costPrice;
+    @Column(name = "selling_price")
     private Double sellingPrice;
+    @Column(name = "learning_resource_status")
+    @Enumerated(EnumType.STRING)
     private LearningResourceStatus learningResourceStatus;
+    @Column(name = "created_date")
     private LocalDate createdDate;
+    @Column(name = "published_date")
     private LocalDate publishedDate;
+    @Column(name = "retired_date")
     private LocalDate retiredDate;
 
 }
--- src/main/java/com/tgt/rysetii/learningresourcesapi/service/LearningResourceService.java
+++ src/main/java/com/tgt/rysetii/learningresourcesapi/service/LearningResourceService.java
@@ -2,6 +2,9 @@ package com.tgt.rysetii.learningresourcesapi.service;
 
 import com.tgt.rysetii.learningresourcesapi.entity.LearningResource;
 import com.tgt.rysetii.learningresourcesapi.entity.LearningResourceStatus;
+import com.tgt.rysetii.learningresourcesapi.repository.LearningResourceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.time.LocalDate;
@@ -10,83 +13,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class LearningResourceService {
-    public List<LearningResource> getLearningResources(){
-        File file = new File("LearningResources.csv");
-        List<LearningResource> learningResources = loadLearningResourceFromCsv(file);
-        return learningResources;
-    }
 
+    @Autowired
+    private LearningResourceRepository learningResourceRepository;
 
-    private List<LearningResource> loadLearningResourceFromCsv(File file) {
-        List<LearningResource> learningResources = new ArrayList<>();
-        try {
-            FileReader reader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(reader);
-
-            String line = null;
-            line = bufferedReader.readLine();
-            while (line!=null){
-                String[] attributes = line.split(",");
-                LearningResource learningResource = createLearningResource(attributes);
-                learningResources.add(learningResource);
-                line = bufferedReader.readLine();
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return learningResources;
-    }
-
-    private LearningResource createLearningResource(String[] attributes) {
-        Integer id = Integer.parseInt(attributes[0].replaceAll("\\D+", ""));
-        String resourceName = attributes[1];
-        Double costPrice = Double.parseDouble(attributes[2]);
-        Double sellingPrice = Double.parseDouble(attributes[3]);
-        LearningResourceStatus learningResourceStatus = LearningResourceStatus.valueOf(attributes[4]);
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        LocalDate createdDate = LocalDate.parse(attributes[5],dateTimeFormatter);
-        LocalDate publishedDate = LocalDate.parse(attributes[6],dateTimeFormatter);
-        LocalDate retiredDate = LocalDate.parse(attributes[7],dateTimeFormatter);
-        LearningResource learningResource = new LearningResource(id,resourceName,costPrice,sellingPrice,learningResourceStatus,createdDate,publishedDate,retiredDate);
-        return learningResource;
-    }
     public void saveLearningResources(List<LearningResource> learningResources){
-        populateLearningResourcesToCsv(learningResources);
-    }
-
-    private void populateLearningResourcesToCsv(List<LearningResource> learningResources){
-        final String delimiter = ",";
-        try {
-            FileWriter file = new FileWriter("LearningResources.csv",true);
-            BufferedWriter bufferedWriter = new BufferedWriter(file);
-            for(LearningResource learningResource:learningResources){
-                bufferedWriter.newLine();
-                StringBuffer line = new StringBuffer();
-                line.append(learningResource.getId());
-                line.append(delimiter);
-                line.append(learningResource.getProductName());
-                line.append(delimiter);
-                line.append(learningResource.getCostPrice());
-                line.append(delimiter);
-                line.append(learningResource.getSellingPrice());
-                line.append(delimiter);
-                line.append(learningResource.getLearningResourceStatus());
-                line.append(delimiter);
-                line.append(learningResource.getCreatedDate());
-                line.append(delimiter);
-                line.append(learningResource.getPublishedDate());
-                line.append(delimiter);
-                line.append(learningResource.getRetiredDate());
-                line.append(delimiter);
-            }
-            bufferedWriter.flush();
-            bufferedWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        for(LearningResource learningResource: learningResources){
+            learningResourceRepository.save(learningResource);
         }
     }
+
+    public List<LearningResource> getLearningResources(){
+        return learningResourceRepository.findAll();
+    }
+
 
     public List<Double> getProfitMargin(){
         List<LearningResource> learningResources = getLearningResources();
